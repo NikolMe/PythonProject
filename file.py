81617f1a15b78e889e37531e3edc3de58c1485b2
Oleh Korchan <oleh_korchan@epam.com>
2022-05-19 10:22:24 +0300

    nikole changes

--- file.py
+++ file.py
@@ -1,7 +1,26 @@
 from operator import contains
 
 
-print("Введите ребра графа")
+def matrix_A(U, X):
+    print("  ", end="")
+    for i in range(1, len(U)+1):
+        print(i, end=" ")
+    print()
+    for i in X:
+        print(i, end=" ")
+        for j in U:
+            if i in j:
+                print(1, end=" ")
+            else:
+                print(0, end=" ")
+        print()
+
+
+def matrix_B(U, X):
+    
+
+
+print("Enter:")
 U = input().split(" ")
 X = []
 for i in U:
@@ -11,3 +30,4 @@ for i in U:
         X.append(i[3])
 X = sorted(X)
 print(X)
+matrix_A(U, X)
